2387bb442cda5ed59a16dffe8b1d459ec62ab4b9
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-07-25 16:36:38 +0530

    New Backend page

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const mongoDB = require('./database'); 
+const path = require('path');
+const mongoDB = require('./database');
 
 const app = express();
 const port = 5000;
 
 //CORS
-app.use(
-  (req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "http://localhost:3000"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  }
-)
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    // "http://localhost:3000"
+    "https://food-way.vercel.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get('/', (req, res) => { 
-  res.send("Backend server is running!");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './pages/backend-home.html'));
 });
 
 app.use(express.json());
